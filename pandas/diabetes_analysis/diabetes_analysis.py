aef61236f9ff42b93a7bf79054328951164e908a
maria <fernandomontilla@gmail.com>
2020-05-18 16:07:15 -0500

    Unnecessary files are removed

--- pandas/diabetes_analysis/diabetes_analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-# Se crea el dataframe data_diabetes
-data_diabetes = pd.read_csv("~/data/Diabetes_es_ext.csv")
-
-data_diabetes.head()
