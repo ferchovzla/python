4b54ed25f97183b921128d9a06a366933f8a03b2
maria <fernandomontilla@gmail.com>
2020-05-26 11:49:20 -0500

    Add validate email code with regular expression (import re)

---
+++ validate_email/validate_email.py
@@ -0,0 +1,34 @@
+# Python program to validate an Email 
+
+# re module provides support 
+# for regular expressions 
+import re 
+
+# Regular expression for validating an Email 
+regex = '^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$'
+# for custom mails use: '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$' 
+	
+# Function for validating an Email 
+def validate_email(email): 
+
+	# use the match function with pattern regex and email 
+	if(re.search(regex,email)): 
+		print(email+": (_/) VALID") 
+	else: 
+		print(email+": (X) INVALID") 
+	
+ 
+if __name__ == '__main__' : 
+	
+	emails = ["fernandomontilla@gmail.com", \
+	          "fernando.montilla.02@gmail.com", \
+	          "fernando.@montilla.02@gmail.com", \
+	          "my.ownsite@mydomain.org",\
+	          "my.$%wrongname@mydomain.net",\
+	          "venezuela.info",\
+	          "ferchodeveloper@myempresa.com.ve"]
+
+	for email in emails:
+		validate_email(email)
+
+    
